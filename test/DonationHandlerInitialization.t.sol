0dddb102bbb9a149e6664089ef28e36d2f6a2dba
Kurt <Kurt@Merbeth.io>
2022-10-15 14:44:40 +0200

    added initialization test

---
+++ test/DonationHandlerInitialization.t.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "./shared/SharedInitialization.sol";
+
+contract DonationHandlerInitializationTest is SharedInitialization {
+    function setUp() public override {
+        _initializeVariables();
+    }
+
+    function test_initialization() public {
+        _initializeDonationHandler();
+
+        assertEq(allowedToken.balanceOf(deployer), 1_000_000 * 1e18);
+        assertFalse(donationHandler.isTokenAccepted(address(notAllowedToken)));
+        assertTrue(donationHandler.isTokenAccepted(address(allowedToken)));
+        assertTrue(donationHandler.isDonationRecipient(address(1)));
+        assertTrue(donationHandler.isFeeReceiver(address(2)));
+        assertTrue(donationHandler.isAdmin(address(3)));
+    }
+
+     function testFail_initializationSecondTime() public {
+        _initializeDonationHandler();
+        _initializeDonationHandler();
+    }
+}
