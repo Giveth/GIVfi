da31483890c122f27615dd6dfc5e2caa452716b0
Kurt <Kurt@Merbeth.io>
2022-12-04 17:24:48 +0100

    removed multi call

--- src/DonationHandler.sol
+++ src/DonationHandler.sol
@@ -4,7 +4,6 @@ pragma solidity 0.8.17;
 import {IERC20Upgradeable as IERC20} from "@openzeppelin/contracts-upgradeable/token/ERC20/IERC20Upgradeable.sol";
 import {SafeERC20Upgradeable as SafeERC20} from "@openzeppelin/contracts-upgradeable/token/ERC20/utils/SafeERC20Upgradeable.sol";
 import {ReentrancyGuardUpgradeable as ReentrancyGuard} from "@openzeppelin/contracts-upgradeable/security/ReentrancyGuardUpgradeable.sol";
-import {MulticallUpgradeable as Multicall} from "@openzeppelin/contracts-upgradeable/utils/MulticallUpgradeable.sol";
 import "./DonationHandlerRoles.sol";
 import "./gitcoin/IVotingStrategy.sol";
 
@@ -34,8 +33,7 @@ import "./gitcoin/IVotingStrategy.sol";
 contract DonationHandler is
     IVotingStrategy,
     DonationHandlerRoles,
-    ReentrancyGuard,
-    Multicall
+    ReentrancyGuard
 {
     using SafeERC20 for IERC20;
 
@@ -70,7 +68,6 @@ contract DonationHandler is
             _admins
         );
         __ReentrancyGuard_init();
-        __Multicall_init();
 
         if (_acceptedToken.length > 0) {
             isTokenWhitelistActive = true;
